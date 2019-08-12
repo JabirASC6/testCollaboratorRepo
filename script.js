7356083ce0837886a84f98e36a8e734689f9558d
AidanASC6 <aegislash990@gmail.com>
2019-08-12 11:59:21 -0400

    HTML structure finished the basic mission statement section

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 let hello = "there";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b5cb027cb9f1bbedcebd5d9c5adb6cc30149cbc0
 let general = "kenobi"
 let general = "grievious";
-<<<<<<< HEAD
->>>>>>> 27185d6577f0f42c3acca04d184c8c73003cf900
-=======
+
 let general = "grievious";
 
 
 let branch="change";
->>>>>>> secondBranch
-=======
->>>>>>> b5cb027cb9f1bbedcebd5d9c5adb6cc30149cbc0
+ secondBranch
+
