d15467f1060181dc9919683bcbbf02b090b336e9
AidanASC6 <aegislash990@gmail.com>
2019-08-13 09:33:13 -0400

    making everything update tuesday demo week

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let hello = "there";
+//This code is only for demp presentations to switch divs before it is set up
+let presentSearch = document.getElementById("presentSearch");
+let search = document.getElementById("search");
+let home = document.getElementById("homePage");
+let presentHome = document.getElementById("presentHome");
 
-let general = "kenobi"
-let general = "grievious";
+//making presentSearch switch from the home div to the search div
+presentSearch.onclick = function(event){
+  event.preventDefault();
+  home.style.display = "none";
+  search.style.display = "block";
+}
 
-let general = "grievious";
-
-
-let branch="change";
- secondBranch
-
+//making presentHome switch from the search div to the home div
+presentHome.onclick = function(event){
+  event.preventDefault();
+  home.style.display = "block";
+  search.style.display = "none";
+}
